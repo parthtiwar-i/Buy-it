85b0b057d83cb2f626166e3ba8e05b9666bde9a1
Parth Tiwari <tiwariparth02@gmail.com>
2023-09-07 20:34:56 +0530

    backend

---
+++ backend/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51Nmf5gSFug6SIV6tyClhYOYz3JqbX6HTApRP9bePw1T7vrLVvNajq4MLChE4tH6H26ITDSvIDEgM0G1L4sH8Y9of00zEJybSUt"
+);
+
+const app = express();
+
+const corsOptions = {
+    // Replace with your Firebase app's URL
+  origin: "http://localhost:3000", 
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("welsome to localhost server 3000");
+});
+
+app.post("/payments/create", async (req, res) => {
+  const total = req.query.total;
+  // const total = req.query.total;
+   console.log("payment request reciveed boom !!! ", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "INR",
+  });
+
+  res.status(201).json({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// try {
+//   const paymentIntent = await stripe.paymentIntents.create({
+//     amount: total,
+//     currency: "INR",
+//   });
+
+//   res.status(201).send({
+//     clientSecret: paymentIntent.client_secret,
+//   });
+// } catch (error) {
+//   res.status(500).send({
+//     error: error.message,
+//   });
+// }
+//   });
+
+app.listen(process.env.PORT || "3001", (req, res) => {
+  console.log("server is running at port 3000");
+});
